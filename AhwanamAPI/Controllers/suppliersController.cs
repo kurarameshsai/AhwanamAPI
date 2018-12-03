3be0dfc07072ee4a865fe646333f2f5f81b248dc
lakshmixsilica <lakshmi.p@xsilica.com>
2018-12-03 18:57:10 +0530

    latest

---
+++ AhwanamAPI/Controllers/suppliersController.cs
@@ -0,0 +1,37 @@
+﻿using MaaAahwanam.Models;
+using MaaAahwanam.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AhwanamAPI.Custom;
+
+
+namespace AhwanamAPI.Controllers
+{
+    
+    public class suppliersController : ApiController
+    {
+        Vendormaster vendorMaster = new Vendormaster();
+        private static TimeZoneInfo INDIAN_ZONE = TimeZoneInfo.FindSystemTimeZoneById("India Standard Time");
+        VendorDashBoardService mngvendorservice = new VendorDashBoardService();
+        newmanageuser newmanageuse = new newmanageuser();
+
+        [HttpGet]
+        [AllowAnonymous]
+
+        [Route("api/suppliers/GetAllSuppliers/{VendorId}")]
+        public IHttpActionResult GetAllSuppliers(string VendorId)
+        {
+            //var user = (CustomPrincipal)System.Web.HttpContext.Current.User;
+            //string uid = user.UserId.ToString();
+            //string vemail = newmanageuse.Getusername(long.Parse(uid));
+            //vendorMaster = newmanageuse.GetVendorByEmail(vemail);
+            //VendorId = vendorMaster.Id.ToString();
+            var SupplierServicesLst = mngvendorservice.getsupplierservices(VendorId);
+            return Ok(SupplierServicesLst);
+        }
+    }
+}
---
+++ AhwanamAPI/Custom/CustomPrincipal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Security.Principal;
+
+namespace AhwanamAPI.Custom
+{
+    public class CustomPrincipal:IPrincipal
+    {
+        public IIdentity Identity { get; private set; }
+        public CustomPrincipal(string userId)
+        {
+            this.Identity = new GenericIdentity(userId);
+        }
+        public bool IsInRole(string role)
+        {
+            if (Roles.Any(role.Contains))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public string[] Roles { get; set; }
+        public long UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserType { get; set; }
+        public string Username { get; set; }
+    }
+}
