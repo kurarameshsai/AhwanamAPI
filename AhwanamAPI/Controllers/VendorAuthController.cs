5a52d067b909f22b009992f307296e8bb08087d2
sireesh kantamaneni <sireesh.k@xsilica.com>
2018-12-03 16:58:11 +0530

    Vendor Authentication

---
+++ AhwanamAPI/Controllers/VendorAuthController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MaaAahwanam.Models;
+using MaaAahwanam.Service;
+
+namespace AhwanamAPI.Controllers
+{
+    public class VendorAuthController : ApiController
+    {
+        ResultsPageService resultsPageService = new ResultsPageService();
+        [HttpGet]
+        public UserLogin Authenticate()
+        {
+            UserLogin userLogin = new UserLogin();
+            userLogin = resultsPageService.GetUserLogin(userLogin);
+            return userLogin;
+        }
+    }
+}
