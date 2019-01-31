0c304f0bd0fba82f6a16e28def73d44dd8021fd8
lakshmixsilica <lakshmi.p@xsilica.com>
2019-01-31 18:06:54 +0530

    latest

--- AhwanamAPI/Controllers/UserAuthController.cs
+++ AhwanamAPI/Controllers/UserAuthController.cs
@@ -259,7 +259,7 @@ namespace AhwanamAPI.Controllers
                 string readFile = File.OpenText().ReadToEnd();
                 readFile = readFile.Replace("[ActivationLink]", url);
                 readFile = readFile.Replace("[name]", Capitalise(userdetails.FirstName));
-               /* TriggerEmail(userLogin.UserName, readFile, "Your Password is changed", null);*/ // A mail will be triggered
+                TriggerEmail(userLogin.UserName, readFile, "Your Password is changed", null); // A mail will be triggered
                 return Json("success");
             }
             catch (Exception)
@@ -283,7 +283,7 @@ namespace AhwanamAPI.Controllers
                 string readFile = File.OpenText().ReadToEnd();
                 readFile = readFile.Replace("[ActivationLink]", url);
                 readFile = readFile.Replace("[name]", Capitalise(userdetails.FirstName));
-               /* TriggerEmail(Email, readFile, "Password reset information", null);*/ // A mail will be triggered
+               TriggerEmail(Email, readFile, "Password reset information", null);// A mail will be triggered
                 return Json("success");
             }
             return Json("success1");
