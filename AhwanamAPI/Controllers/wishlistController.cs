c5fc57ae4ef3d121a5d798c2747b537735c03499
lakshmixsilica <lakshmi.p@xsilica.com>
2019-03-18 19:54:02 +0530

    latest

--- AhwanamAPI/Controllers/wishlistController.cs
+++ AhwanamAPI/Controllers/wishlistController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MaaAahwanam.Models;
+using MaaAahwanam.Service;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -10,7 +12,7 @@ namespace AhwanamAPI.Controllers
     public class wishlistController : ApiController
     {
 
-        public class wishlist
+        public class wishlistItems
         {
             public long UserId { get; set; }
             public string Name { get; set; }
@@ -18,17 +20,10 @@ namespace AhwanamAPI.Controllers
             public DateTime? StartDate { get; set; }
             public DateTime? EndDate { get; set; }
             public string Description { get; set; }
-            public DateTime UpdatedDate { get; set; }
-        }
-        public class wishlistItems
-        {
-            public long wishlistId { get; set; }
             public long vendorId { get; set; }
             public long vendorsubId { get; set; }
             public string BusinessName { get; set; }
             public string servicetype { get; set; }
-            public long UserId { get; set; }
-            public DateTime WhishListedDate { get; set; }
             public string IPAddress { get; set; }
             public string Status { get; set; }
             public long NotesId { get; set; }
@@ -37,6 +32,41 @@ namespace AhwanamAPI.Controllers
             public DateTime? EventStartDate { get; set; }
             public DateTime? EventEndDate { get; set; }
             public long collabratorid { get; set; }
+        }
+
+        [HttpPost]
+        [Route("api/wishlist/Addwishlist")]
+        public IHttpActionResult Addwishlist(wishlistItems list)
+        {
+            WhishListService wishlistservice = new WhishListService();
+            wishlist wishlists = new wishlist();
+            Userwishlist userwishlistdetails = new Userwishlist();
+            Dictionary<string, object> dict = new Dictionary<string, object>();
+            TimeZoneInfo INDIAN_ZONE = TimeZoneInfo.FindSystemTimeZoneById("India Standard Time");
+            wishlists.StartDate= userwishlistdetails.EventStartDate = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, INDIAN_ZONE);
+            wishlists.EndDate = userwishlistdetails.EventEndDate = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, INDIAN_ZONE);
+            wishlists.UserId = list.UserId;
+            wishlists.Name = list.Name;
+            userwishlistdetails.vendorId = list.vendorId;
+            userwishlistdetails.vendorsubId = list.vendorsubId;
+            userwishlistdetails.BusinessName = list.BusinessName;
+            userwishlistdetails.servicetype = list.servicetype;
+            userwishlistdetails.Ceremony = list.Ceremony;
+            userwishlistdetails.Budjet = list.Budjet;
+            userwishlistdetails.collabratorid = list.collabratorid;
+            var responce = "";
+            responce = wishlistservice.AddAllwishlist(wishlists, userwishlistdetails);
+            if (responce == "success")
+            {
+                dict.Add("status", true);
+                dict.Add("message", "Success");
+            }
+            else
+            {
+                dict.Add("status", false);
+                dict.Add("message", "failed");
+            }
+            return Json(dict);
         }
     }
 }
